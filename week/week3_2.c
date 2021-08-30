94929b0d3d04b24369aeb469ce077d625b7f4912
milenter08@gmail.com <milenter08@gmail.com>
2021-08-30 18:19:31 +0700

    ;

--- week/week3_2.c
+++ week/week3_2.c
@@ -3,12 +3,17 @@ int main(){
 	int a,num,max=0,min=9999999;
     for(a=1;a<=3;a++)
     {
-        printf("Enter Number : ",i);
+        printf("Enter Number : ",a);
         scanf("%d",&num);
         if(max < num)   max=num;
         if(num < min)   min=num;
     }
-    printf("The Biggest Number is  : %d \n",max);
-    printf("The Smallest Number is : %d \n",min);
+    if(max==min){
+    	printf("No Biggest And Smallest Number");
+    }
+    else if(max!=min){
+    	printf("The Biggest  Number is : %d \n",max);
+    	printf("The Smallest Number is : %d \n",min);
+    }
     return 0;
 }
